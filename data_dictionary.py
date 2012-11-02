23b85cfdf57ed751fbbe2972167e356b1eb6a40a
Alexander Golec <thejfasi@gmail.com>
2012-11-02 15:05:37 -0400

    Intial commit.

---
+++ data_dictionary.py
@@ -0,0 +1,113 @@
+'''
+Methods for reading and parsing the data dictionary specification file.
+'''
+
+from sys import stderr
+
+class DataSpec:
+    def __init__(self, name, start, end, value_min=None, value_max=None):
+        self.name = name
+
+        if start > end:
+            raise ValueError('invalid position')
+
+        self.start = start
+        self.end = end
+
+        if (value_min is not None and value_max is None or
+            value_max is not None and value_min is None):
+            raise ValueError('either none or both of value_min and value_max must be set')
+        if (value_min is not None and value_max is not None
+            and value_min > value_max):
+            raise ValueError('invalid range')
+            
+        self.value_min = value_min
+        self.value_max = value_max
+
+def extract_range(line):
+    '''
+    Extract the first singly-parenthesized item from the string. Nested
+    parentheses are not supported because they are unnecessary for this
+    application, and will throw. Return values are:
+     - the contents of the first parenthesized block, without parentheses
+     - the original string without the parenthesized block
+    '''
+    inside = False
+    ret = []
+    start = None
+    end = None
+    for i in xrange(0, len(line)):
+        char = line[i]
+        if inside is False:
+            if char == '(':
+                start = i
+                inside = True
+        elif inside is True:
+            # we assume there is only one layer of nesting
+            assert char != '('
+            if char != ')':
+                # this will be at least one, since we can only enter the
+                # inside state after seeing at least one character
+                ret.append(char)
+            else:
+                end = i
+                break
+    if start is not None and end is not None:
+        # start is exclusive, so dont' add to it, but end is inclusive, so add
+        return (''.join(ret), line[0:start]+line[end + 1:len(line)])
+    else:
+        return ('', line)
+
+def data_spec_line(line):
+    '''
+    Turn a line into a data spec object.
+    '''
+    line_before = line
+    # extract the value range before splitting the line
+    value_range, line = extract_range(line)
+    line = line.strip()
+
+    # a little sanity checking
+    if value_range != '':
+        assert line+' ('+value_range+')' == line_before, line_before
+    else:
+        assert line == line_before
+
+    kind, name, length, start = line.split(' ')
+    length = int(length)
+    start = int(start) - 1
+    value_min = None
+    value_max = None
+
+    if value_range != '':
+        split = value_range.split(' ')
+        assert len(split) == 1 or len(split) == 2
+        if len(split) == 2:
+            val_range, auxiliary = split
+        else:
+            val_range = split[0]
+            auxiliary = None
+
+        value_min, value_max = val_range.split(':')
+        value_min = int(value_min)
+        value_max = int(value_max)
+
+    return DataSpec(name, start, start+length, value_min, value_max)
+
+import re
+
+def extract_columns(f):
+    linenum = 0
+    spec = []
+    for line in f.xreadlines():
+        linenum += 1
+        line = re.sub(r' +', ' ', line.strip())
+        if len(line) < 2:
+            print >> stderr, linenum, 'length of line too short:', line
+            continue
+        elif line[0] == 'D' and line[1] == ' ':
+            spec.append(data_spec_line(line))
+
+if __name__ == '__main__':
+    with open('asec2012early_pubuse.dd.txt', 'r') as f:
+        extract_columns(f)
---
+++ test.py
@@ -0,0 +1,78 @@
+import unittest
+
+###############################################################################
+# DataSpec
+
+from data_dictionary import DataSpec
+
+class test_DataSpec(unittest.TestCase):
+    def test_none_min(self):
+        self.assertRaises(ValueError, DataSpec, 'ABC', 0, 10, 0, None)
+    def test_none_max(self):
+        self.assertRaises(ValueError, DataSpec, 'ABC', 0, 10, None, 0)
+    def test_no_range(self):
+        try:
+            DataSpec('ABC', 0, 10)
+        except ValueError:
+            self.fail()
+    def test_full_range(self):
+        try:
+            DataSpec('ABC', 0, 10, 0, 100)
+        except ValueError:
+            self.fail()
+    def test_invalid_range(self):
+        self.assertRaises(ValueError, DataSpec, 'ABC', 0, 10, 100, 0)
+    def test_invalid_position(self):
+        self.assertRaises(ValueError, DataSpec, 'ABC', 10, 0)
+
+###############################################################################
+# extract_range
+
+from data_dictionary import extract_range
+
+class test_extract_range(unittest.TestCase):
+    def test_only_parens(self):
+        self.assertEqual(extract_range('(a, b, c)'), ('a, b, c', ''))
+    def test_multiple_parens(self):
+        self.assertEqual(extract_range('(a, b, c)(d, e, f)'),
+                         ('a, b, c', '(d, e, f)'))
+    def test_raise_on_nested_parens(self):
+        self.assertRaises(AssertionError, extract_range, '(())')
+    def test_part_of_string_after(self):
+        self.assertEqual(extract_range('a b c (1 2 3) d e f'),
+                         ('1 2 3', 'a b c  d e f'))
+    def test_no_parens(self):
+        self.assertEqual(extract_range('a b c d e f'),
+                         ('', 'a b c d e f'))
+    def test_unclosed_left(self):
+        self.assertEqual(extract_range('a b c ( d e f'),
+                         ('', 'a b c ( d e f'))
+    def test_start_on_paren(self):
+        self.assertEqual(extract_range('(a b c d) e f g'),
+                         ('a b c d', ' e f g'))
+
+###############################################################################
+# data_spec_line
+
+from data_dictionary import data_spec_line
+from data_dictionary import DataSpec
+
+class test_data_spec_line(unittest.TestCase):
+    def test_meta_test_equality_func(self):
+        self.assertTrue(self.equality(DataSpec('FILLER', 58, 59),
+                                      DataSpec('FILLER', 58, 59)))
+    def test_simple_line(self):
+        self.assertTrue(self.equality(data_spec_line('D FILLER 1 59'),
+                                       DataSpec('FILLER', 58, 59)))
+    def test_simple_range(self):
+        self.assertTrue(self.equality(data_spec_line('D MIG_DSCP 1 328 (0:5)'),
+                                      DataSpec('MIG_DSCP', 327, 328, 0, 5)))
+    def equality(self, ds1, ds2, msg=None):
+        return (ds1.name == ds2.name and
+            ds1.start == ds2.start and
+            ds1.end == ds2.end and
+            ds1.value_min == ds2.value_min and
+            ds1.value_max == ds2.value_max)
+
+if __name__ == '__main__':
+    unittest.main()
